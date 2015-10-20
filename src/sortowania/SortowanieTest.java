208f0eb074f1f0c940ec3a260ae90dcd2b128934
Paweł <drapek39@wp.pl>
2015-10-20 13:03:11 +0400

    dodanie przypadków best i worst do merge sort, czyli osiągnięto wszystkie założenia projektowe

--- src/sortowania/SortowanieTest.java
+++ src/sortowania/SortowanieTest.java
@@ -48,42 +48,42 @@ public class SortowanieTest {
         System.out.println();
         
 
-        System.out.println("Sortowanie tablicy pesimistycznej (posrotowanej w odwrotym kierunku): ");
+        System.out.println("Sortowanie tablicy pesimistycznej (porozrzucanej według specjalnego algorytmu): ");
         testujSortowanie("merge", "worst", startArrayLength, numberRepeatsOfSortingArrays, incrementNextArrayLength, printsArrays, printTimesInCSV);
         System.out.println();
+        
+        
     }
     
     private static void testujSortowanie(String typSortowania, String wybranyPrzypadek, int arrayLength, int numberRepeatsOfSortingArrays, int incrementNextArrayLength,
                                          boolean printsArrays, boolean printTimesInCSV) {
         for(int n = 0; n < numberRepeatsOfSortingArrays; n++) {
         
-            double[] tablicaInsert = new double[arrayLength];
+            double[] tablicaWartosci = new double[arrayLength];
             
             if (wybranyPrzypadek.equalsIgnoreCase("random")) {
                 Random rnd = new Random();
                 for (int i = 0; i < arrayLength; i++) {
-                    tablicaInsert[i] = rnd.nextDouble();
+                    tablicaWartosci[i] = rnd.nextDouble();
                 }
             } else if(wybranyPrzypadek.equalsIgnoreCase("best")) {
-                if (typSortowania.equalsIgnoreCase("insert")) {
-                    for (int i = 0; i < arrayLength; i++) {
-                        tablicaInsert[i] = i*1.32;
-                    }
-                } else if (typSortowania.equalsIgnoreCase("merge")) {
-                    //TODO add best merge case
-                    throw new UnsupportedOperationException("Not supported yet.");
-                } else {
-                    System.out.println("Podano zły typ sortowania, dostępne to merge i insert!");
-                    return;
-                }
+               
+                for (int i = 0; i < arrayLength; i++) {
+                    tablicaWartosci[i] = i*1.32;
+                } 
+                
             } else if(wybranyPrzypadek.equalsIgnoreCase("worst")) {
                 if (typSortowania.equalsIgnoreCase("insert")) {
                     for (int i = 0; i < arrayLength; i++) {
-                        tablicaInsert[i] = arrayLength - i;
+                        tablicaWartosci[i] = arrayLength - i;
                     }
                 } else if (typSortowania.equalsIgnoreCase("merge")) {
-                    //TODO add worst merge case
-                    throw new UnsupportedOperationException("Not supported yet.");
+                    //make sorted array
+                    for (int i = 0; i < arrayLength; i++) {
+                        tablicaWartosci[i] = i;
+                    }
+                    makeMergeWorstCaseArr(tablicaWartosci);
+                    
                 } else {
                     System.out.println("Podano zły typ sortowania, dostępne to merge i insert!");
                     return;
@@ -95,7 +95,7 @@ public class SortowanieTest {
             
             if (printsArrays) {
                 System.out.println("Tablica przed: ");
-                for (double elem : tablicaInsert) {
+                for (double elem : tablicaWartosci) {
                     System.out.println("    " + elem);
                 }
             }
@@ -105,14 +105,14 @@ public class SortowanieTest {
                 InsertSort insertSort = new InsertSort();
                 System.gc(); //uruchomienie garbage collectora, by oczyścić pamięć przed
                 long startTime = System.nanoTime();
-                tablicaInsert = insertSort.sort(tablicaInsert);
+                tablicaWartosci = insertSort.sort(tablicaWartosci);
                 estimatedTime = System.nanoTime() - startTime;
                 
             } else if( typSortowania.equalsIgnoreCase("merge")) {
                 InsertSort insertSort = new InsertSort();
                 System.gc(); //uruchomienie garbage collectora, by oczyścić pamięć przed
                 long startTime = System.nanoTime();
-                tablicaInsert = insertSort.sort(tablicaInsert);
+                tablicaWartosci = insertSort.sort(tablicaWartosci);
                 estimatedTime = System.nanoTime() - startTime;
             } else {
                 System.out.println("Błąd przy rozpoznawaniu nazwy algorytmu sortowania!");
@@ -121,7 +121,7 @@ public class SortowanieTest {
             
             if (printsArrays) {
                 System.out.println("Tablica po: ");
-                for (double elem : tablicaInsert) {
+                for (double elem : tablicaWartosci) {
                     System.out.println("    " + elem);
                 }
             }
@@ -135,4 +135,42 @@ public class SortowanieTest {
         }
     }
     
+
+    
+    public static void makeMergeWorstCaseArr(double[] arr) { 
+
+            if(arr.length<=1)
+                return;
+
+            if(arr.length==2)
+            {
+                double swap=arr[0];
+                arr[0]=arr[1];
+                arr[1]=swap;
+                return;
+            }
+
+        int i,j;
+        int m = (arr.length + 1) / 2;
+        double left[] = new double[m];
+        double right[] = new double[arr.length-m];
+
+        for(i=0,j=0;i<arr.length;i=i+2,j++) //Storing alternate elements in left subarray
+            left[j]=arr[i];
+
+        for(i=1,j=0;i<arr.length;i=i+2,j++) //Storing alternate elements in right subarray
+            right[j]=arr[i];
+
+        makeMergeWorstCaseArr(left);
+        makeMergeWorstCaseArr(right);
+        mergeTwoarrIntoOne(arr, left, right);
+    }
+    
+     public static void mergeTwoarrIntoOne(double[] arr, double[] left, double[] right) {
+        int i,j;
+        for(i=0;i<left.length;i++)
+                arr[i]=left[i];
+        for(j=0;j<right.length;j++,i++)
+                arr[i]=right[j];
+    }
 }
